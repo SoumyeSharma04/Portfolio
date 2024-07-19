0c7667428637a02bf2e313dff7621d65de7cd070
SoumyeSharma04 <soumye44@gmail.com>
2024-07-19 19:59:33 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import logo from "../assets/logo.jpeg";
+import { FaLinkedin, FaGithub,  FaFile } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <nav className=" mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img className="mx-2 w-16" src={logo} alt="logo"></img>
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a
+          href="https://drive.google.com/file/d/19wzvdu0XTOoFBeHL3hnc9oacs7ERhiuC/view?usp=drive_link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaFile />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/soumyesharma04/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://github.com/SoumyeSharma04"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub />
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/constants/index.js
@@ -0,0 +1,47 @@
+import project1 from "../assets/projects/Ascend.png";
+import project2 from "../assets/projects/ecommerce.png";
+
+export const HERO_CONTENT = `As a passionate student at IIT Jammu with a solid foundation in full stack development and a keen interest in AWS, I bring hands-on experience in developing and managing web applications. I aim to leverage my skills to address real-world challenges and deliver impactful solutions.`;
+
+export const ABOUT_TEXT = `Hello, I'm Soumye Sharma, an experienced Software Developer with a strong background in developing scalable web applications, currently pursuing a B.Tech in Civil Engineering with a minor in Computer Science from IIT Jammu. Proficient in C++, I've built responsive websites and optimized digital workflows during internships at BharatMaker and Nucleon. My projects include an e-commerce platform and a personalized student performance improvement tool. I excel in leveraging technologies like React, Node.js, Express.js, MongoDB, and AWS. Beyond coding, I'm a sports enthusiast and a Black Belt in Shotokan Karate, with multiple national championships. Let's connect to create impactful digital solutions together.`;
+
+export const EXPERIENCES = [
+  {
+    year: "May 2024 - July 2024",
+    role: "Full Stack Developer Intern",
+    company: "BharatMaker",
+    description: `The platform connects customers with top-tier 3D printing service providers, offering solutions for rapid prototyping and customized projects. Ensured SEO best practices to enhance search engine visibility and ranking. Additionally, created and managed an EC2 instance on AWS, configured Route53 for domain management, and utilized S3 buckets for scalable and secure storage solutions.`,
+    technologies: ["ReactJS", "AWS", "ExpressJS", "NodeJS", "MongoDB"],
+  },
+  {
+    year: "Dec 2023 - Jan 2024",
+    role: "Full Stack Developer Intern",
+    company: "Nucleon",
+    description: `Created an all-in-one secure digital platform to facilitate document and data access for the filming team. Engineered an innovative paperless workflow, transforming a 10-day process into a streamlined 5-minute
+ procedure, fostering seamless collaboration and productivity gains.`,
+    technologies: ["ReactJS", "JavaScript", "CSS", "Django"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "University Project Ecosystem Challenge",
+    image: project1,
+    description:
+      "Developed a customized test platform to assess various Tech skills. Implemented personalized roadmaps, leading to a 20% increase in user growth. Collaborated with 8-member cross-functional team to design, develop, and integrate the platform. Prepared a business model through comprehensive questionnaire analysis, ensuring alignment with user needs and market demands.",
+    technologies: ["ReactJS", "Javascript", "MongoDB", "ExpressJS", "NodeJS"],
+  },
+  {
+    title: "E-Commerce Website",
+    image: project2,
+    description:
+      "Architected and developed an e-commerce platform using the MERN stack, ensuring a seamless end-to-end experience for users. Utilized React.js to create dynamic and responsive user interfaces, enhancing user engagement and satisfaction. Employed MongoDB for efficient data storage and retrieval, ensuring scalability and performance as the platform grows.",
+    technologies: ["ReactJS", "Javascript", "MongoDB", "ExpressJS", "NodeJS"],
+  },
+];
+
+export const CONTACT = {
+  address: "Rajasthan, India",
+  phoneNo: "+91 6376801575",
+  email: "soumye44@gmail.com",
+};
